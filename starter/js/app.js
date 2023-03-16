372db6d948580e0c9619612fec65267ec19064b2
Bbenjamil <bbenjamill@icloud.com>
2023-03-16 01:48:55 +0100

    Day2 Pro0.1 bulding Rice online

---
+++ starter/js/app.js
@@ -0,0 +1,21 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('click', () => {
+  links.classList.toggle('show-links')
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
+
+// to who this may concern, i have no idea of how js works at the momnent, so i'm basically pasting from my resource file just to get this done, once i'm done with this project(s), I tackle JS next
